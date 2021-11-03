d92d8006c80573526134340f8902d8df5da425ca
Jan Schiefer <janschiefer@posteo.de>
2021-11-03 17:48:47 +0100

    Added basic gui functionality

--- main.py
+++ main.py
@@ -1,46 +1,117 @@
-import math
-
-
-def gameplay():
-
-    roundtype = ["Prediction", "Result"]
-    # get player count as input, must be 3-6
-    pcount = 0
-    while int(pcount) < 3 or int(pcount) > 6:
-        pcount = input("How many players? ")
-    players = int(pcount)
-    p_data = []
-    for player in range(players):
-        data = {}
-        data["name"] = input(f"What's Player {player}'s name? ")
-        data["points"] = 0
-        data["prediction"] = 0
-        data["result"] = 0
-        p_data.insert(player, data)
-
-    print (p_data)
-    # get card count
-    cards = 60
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import pyqtSignal
+from PyQt5 import uic
+import sys
+from dataclasses import dataclass, field
+from enum import Enum, auto
 
 
 
-    # calculate rounds
-    rounds = math.floor(int(cards) / int(players))
+class MainMenu(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainMenu, self).__init__()
+        uic.loadUi('main-menu.ui', self)
+        self.setWindowTitle("Wizard Tracker - Main Menu")
+        button = self.buttonNewGame
+        button.clicked.connect(self.newGame)
 
-    # start gameplay routine
-    for round in range(rounds):
-        print("Round: " + str(round + 1))
-        for type in roundtype:
-            for player in p_data:
+    def newGame(self):
+        self.newgame = SetPlayers(self)
+        # self.newgame.closed.connect(self.show)
+        self.newgame.show()
+        self.close()
 
-                inp = input(f"{player['name']}'s {type}: ")
+class SetPlayers(QMainWindow):
+
+    closed = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(SetPlayers, self).__init__()
+        uic.loadUi('set-players.ui', self)
+        self.setWindowTitle("Wizard Tracker - Set Players")
+        button = self.pushButton
+        button.setText("Start")
+        button.clicked.connect(self.startGame)
+
+    def startGame(self):
+        self.startgame = GameRound(self)
+        # self.startgame.closed.connect(self.show)
+        self.startgame.show()
+        self.close()
+
+    def closeEvent(self, event):
+        self.closed.emit()
+        event.accept()
+
+class GameRound(QMainWindow):
+
+    closed = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super(GameRound, self).__init__()
+        uic.loadUi('main-game-rounds.ui', self)
+        button = self.pushButton
+        button.setText("Next")
+        button.clicked.connect(self.nextRound)
+
+    def nextRound(self):
+        self.setWindowTitle("Text")
+        trackround(data)
+
+
+    def closeEvent(self, event):
+        self.closed.emit()
+        event.accept()
+
+
+
+class Type(Enum):
+    Prediction = auto()
+    Results = auto()
+
+@dataclass
+class Person:
+    name: str
+    points: int
+    prediction: int
+    result: int
+    rank: int
+
+    def __post_init__(self):
+        self.sort_index = self.rank
+
+    def __str__(self):
+        return f'{self.name} has {self.points} points and is currently on rank {self.rank}'
+
+@dataclass
+class GameData:
+    type: Type
+    roundid: int = 0
+    players: int = 0
+
+
+
+def trackround(data):
+    if data.type == Type.Prediction:
+        data.type = Type.Results
+
+
+    if data.type == Type.Results:
+        data.type = Type.Prediction
+    data.roundid += 1
 
 
 
 
 
+def main():
+    app = QApplication(sys.argv)
+    mainmenu = MainMenu()
+    mainmenu.show()
+    data = GameData(0, Type.Prediction, 0)
+    sys.exit(app.exec_())
 
 
-if __name__ == '__main__':
-    gameplay()
+if __name__ == "__main__":
 
+    main()
