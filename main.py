63e269b1cb12b67458c29b4902d64c122b18637b
Jan Schiefer <janschiefer@posteo.de>
2021-11-03 19:14:46 +0100

    Playerdata is now saved and names are transfered between windows

--- main.py
+++ main.py
@@ -34,6 +34,25 @@ class SetPlayers(QMainWindow):
         button.clicked.connect(self.startGame)
 
     def startGame(self):
+        data.players = 4
+
+        p1name = self.lineEdit.text()
+        p2name = self.lineEdit_2.text()
+        p3name = self.lineEdit_3.text()
+        p4name = self.lineEdit_4.text()
+        print(p1name, p2name)
+
+        p1 = Player(p1name, 0, 0, 0, 0)
+        p2 = Player(p2name, 0, 0, 0, 0)
+        p3 = Player(p3name, 0, 0, 0, 0)
+        p4 = Player(p4name, 0, 0, 0, 0)
+
+        players.append(p1)
+        players.append(p2)
+        players.append(p3)
+        players.append(p4)
+
+        print(players)
         self.startgame = GameRound(self)
         # self.startgame.closed.connect(self.show)
         self.startgame.show()
@@ -53,6 +72,11 @@ class GameRound(QMainWindow):
         button = self.pushButton
         button.setText("Next")
         button.clicked.connect(self.nextRound)
+        self.labelTitle.setText("Round: " + str(data.roundid) + " - " + str(data.type))
+        self.label.setText(players[0].name)
+        self.label_2.setText(players[1].name)
+        self.label_3.setText(players[2].name)
+        self.label_4.setText(players[3].name)
 
     def nextRound(self):
         self.setWindowTitle("Text")
@@ -70,7 +94,7 @@ class Type(Enum):
     Results = auto()
 
 @dataclass
-class Person:
+class Player:
     name: str
     points: int
     prediction: int
@@ -102,13 +126,14 @@ def trackround(data):
 
 
 
-
+data = GameData(Type.Prediction, 0, 0)
+players = []
 
 def main():
     app = QApplication(sys.argv)
     mainmenu = MainMenu()
     mainmenu.show()
-    data = GameData(0, Type.Prediction, 0)
+
     sys.exit(app.exec_())
 
 
